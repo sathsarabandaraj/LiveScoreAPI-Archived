3a33beb4f18e469cde47fd46be439b5ac4b8604f
Sathsara Bandara Jayasundara <sathsarabjayasundara@gmail.com>
2023-02-19 20:42:23 +0530

    Add PlayerController with addPlayer post   method

---
+++ Models/Player.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+
+namespace LiveScoreAPI.Models;
+
+public class Player
+{
+    [BsonId]
+    public int id { get; set; }
+    [Required]
+    public bool isHost { get; set; }
+    [Required]
+    public string? name { get; set; }
+    public int age { get; set; }
+    public string? position { get; set; }
+    public int runs { get; set; }
+    public int wickets { get; set; }
+
+    public Player()
+    {
+        age = 0;
+        position = "";
+        runs = 0;
+        wickets = 0;
+    }
+}
